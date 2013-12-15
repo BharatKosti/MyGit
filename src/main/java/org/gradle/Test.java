c16142d4a991d40bb7578447b23bf5959070a1b6
BharatKosti <bharat_teja416@yahoo.com>
2013-12-15 10:37:51 -0800

    Added a comment

--- src/main/java/org/gradle/Test.java
+++ src/main/java/org/gradle/Test.java
@@ -3,6 +3,7 @@ package org.gradle;
 public class Test {
 
 	public static void main(String[] args) {
+		//Testing for git
 		Person person = new Person("Bharat");
 		System.out.println(person.getName());
 	}
